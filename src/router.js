3ac06e420b490057a5271400500345768f0273c6
t--takai <takai01010100@gmail.com>
2018-10-01 13:22:12 +0900

    resolve package

--- src/router.js
+++ src/router.js
@@ -19,5 +19,10 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
+    {
+      path: '/app',
+      name: 'app',
+      component: () => import(/* webpackChunkName: "about" */ './views/App.vue'),
+    },
   ],
 });
